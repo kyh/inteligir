b57dae0e50d08f64b1a35f61cbe852214efdf1c7
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-11-23 23:17:14 -0800

    Rewrote react router routes

--- app/routes.jsx
+++ app/routes.jsx
@@ -1,30 +1,14 @@
 import React from 'react';
 import { Router, Route } from 'react-router';
 import { generateRoute } from 'utils/localized-routes';
+import Routes from 'data/routes';
 
 export default (
   <Router>
     <Route component={ require('./components/app') }>
-      { generateRoute({
-        paths: ['/', '/feed'],
-        component: require('./components/feed/feed')
-      }) }
-      { generateRoute({
-        paths: ['/groups'],
-        component: require('./components/groups/groups')
-      }) }
-      { generateRoute({
-        paths: ['/feed/:id'],
-        component: require('./components/feed/post')
-      }) }
-      { generateRoute({
-        paths: ['/profile'],
-        component: require('./components/profile/profile')
-      }) }
-      { generateRoute({
-        paths: ['/protected'],
-        component: require('./components/protected')
-      }) }
+      <Route path={ Routes.feed } component={ require('./components/feed/feed') }></Route>
+      <Route path={ Routes.groups } component={ require('./components/groups/groups') }></Route>
+      <Route path={ Routes.profile } component={ require('./components/profile/profile') }></Route>
       <Route path='*' component={ require('./pages/not-found') } />
     </Route>
   </Router>
