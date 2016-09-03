653f2857ba9264e0e5029fa93a200a7c51426601
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-09-03 06:01:19 -0700

    Fix copy

--- src/containers/Home/copy.js
+++ src/containers/Home/copy.js
@@ -1,4 +1,4 @@
 export default {
   title: 'Learning is much better with friends',
-  details: 'Create private groups and broadcast your learnings. A place to debate, share knowledge and collaborate because everything is better with community.'
+  details: 'Create private groups and broadcast your learnings. A place to debate, share knowledge, and collaborate because everything is better with community.'
 };
