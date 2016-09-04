0a77f4919e50e87f544f7b2ebee9373c75fb7a50
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-09-04 07:55:00 -0700

    Fix comment

--- src/routes.js
+++ src/routes.js
@@ -37,7 +37,7 @@ export default (store) => {
     function checkAuth() {
       const { auth: { user }} = store.getState();
       if (user) {
-        // oops, not logged in, so can't be here!
+        // Already logged in!
         replace('/feed');
       }
       cb();
