1e02d04809a3e5cb23ff4794c386152d46f4c971
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-09-04 08:09:30 -0700

    Clean up redirect code

--- src/routes.js
+++ src/routes.js
@@ -14,41 +14,28 @@ import {
   } from 'containers';
 
 export default (store) => {
-  // Routes that require user to be logged in
-  const requireLogin = (nextState, replace, cb) => {
-    function checkAuth() {
+  const checkAuthOnEnter = (redirect, isUserLoggedIn) => (nextState, replace, cb) => {
+    function checkUser() {
       const { auth: { user }} = store.getState();
-      if (!user) {
+      if ((isUserLoggedIn && user) || (!isUserLoggedIn && !user)) {
         // oops, not logged in, so can't be here!
-        replace('/');
+        replace(redirect);
       }
       cb();
     }
 
     if (!isAuthLoaded(store.getState())) {
-      store.dispatch(loadAuth()).then(checkAuth, checkAuth);
+      store.dispatch(loadAuth()).then(checkUser, checkUser);
     } else {
-      checkAuth();
+      checkUser();
     }
   };
+
+  // Routes that require user to be logged in
+  const requireLogin = checkAuthOnEnter('/', false);
 
   // Check if user is already logged in, redirect to 'feed' if they are
-  const redirectIfLoggedIn = (nextState, replace, cb) => {
-    function checkAuth() {
-      const { auth: { user }} = store.getState();
-      if (user) {
-        // Already logged in!
-        replace('/feed');
-      }
-      cb();
-    }
-
-    if (isAuthLoaded(store.getState())) {
-      checkAuth();
-    } else {
-      store.dispatch(loadAuth()).then(checkAuth, checkAuth);
-    }
-  };
+  const redirectIfLoggedIn = checkAuthOnEnter('/feed', true);
 
   /**
    * Please keep routes in alphabetical order
