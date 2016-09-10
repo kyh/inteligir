dc4f9c75d8b4345528dc82ef269bfeba2347596d
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-09-10 11:51:21 -0700

    Finish post UI

--- src/containers/NewPost/NewPost.js
+++ src/containers/NewPost/NewPost.js
@@ -9,7 +9,7 @@ const styles = require('./NewPost.scss');
 )
 export default class NewPost extends Component {
   static propTypes = {
-    user: PropTypes.object,
+    user: PropTypes.object
   }
 
   static contextTypes = {
@@ -18,7 +18,10 @@ export default class NewPost extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { editorState: editorStateFromRaw(null) };
+    this.state = {
+      postTitle: '',
+      editorState: editorStateFromRaw(null),
+    };
     this.onChange = ::this.onChange;
   }
 
@@ -30,7 +33,7 @@ export default class NewPost extends Component {
     const router = this.context.router;
 
     return (
-      <div className={styles.newPost}>
+      <section className={styles.newPost}>
         <nav className={styles.newPostNav}>
           <button className={styles.newPostBack} onClick={router.goBack}>Back</button>
           <h1 className={styles.newPostHeaderTitle}>New Post</h1>
@@ -38,13 +41,21 @@ export default class NewPost extends Component {
             <button className={styles.newPostSave}>Publish</button>
           </div>
         </nav>
-        <section className={styles.newPostContent}>
-          <MegadraftEditor
-            editorState={this.state.editorState}
-            onChange={this.onChange}
-          />
+        <section className={styles.newPostContentWrapper}>
+          <section className={styles.newPostContent}>
+            <input
+              className={styles.newPostContentTitle}
+              placeholder="Type your title here"
+              type="text"
+            />
+            <MegadraftEditor
+              editorState={this.state.editorState}
+              onChange={this.onChange}
+              placeholder="Type your post..."
+            />
+          </section>
         </section>
-      </div>
+      </section>
     );
   }
 }
