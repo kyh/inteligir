9b80e438501d2fe5bc6f9e0398853e43f60d5b30
Kaiyu Hsu <im.kaiyu@gmail.com>
2017-05-08 23:37:41 -0700

    Update reducers and clientMiddleware

--- src/redux/create.js
+++ src/redux/create.js
@@ -1,32 +1,52 @@
-import { createStore as _createStore, applyMiddleware, compose } from 'redux';
-import createMiddleware from './middleware/clientMiddleware';
+import { createStore as _createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
+import { createPersistor } from 'redux-persist';
+import createMiddleware from './middleware/clientMiddleware';
+import createReducers from './reducer';
 
-export default function createStore(history, client, data) {
-  // Sync dispatched route actions to the history
-  const reduxRouterMiddleware = routerMiddleware(history);
+export function inject(store, name, asyncReducer) {
+  if (store.asyncReducers[name]) return;
+  store.asyncReducers[name] = asyncReducer;
+  store.replaceReducer(combineReducers(createReducers(store.asyncReducers)));
+}
 
-  const middleware = [createMiddleware(client), reduxRouterMiddleware];
+function getMissingReducers(reducers, data) {
+  if (!data) return {};
+  return Object.keys(data).reduce(
+    (prev, next) => (reducers[next] ? prev : { ...prev, [next]: (state = {}) => state }),
+    {}
+  );
+}
 
-  let finalCreateStore;
-  if (__DEVELOPMENT__ && __CLIENT__ && __DEVTOOLS__) {
+export default function createStore(history, { client, app, restApp }, data, persistConfig = null) {
+  const middleware = [createMiddleware({ client, app, restApp }), routerMiddleware(history)];
+
+  let enhancers = [applyMiddleware(...middleware)];
+  if (__CLIENT__ && __DEVTOOLS__) {
     const { persistState } = require('redux-devtools');
     const DevTools = require('../containers/DevTools/DevTools');
-    finalCreateStore = compose(
-      applyMiddleware(...middleware),
+    enhancers = [
+      ...enhancers,
       window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument(),
       persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
-    )(_createStore);
-  } else {
-    finalCreateStore = applyMiddleware(...middleware)(_createStore);
+    ];
   }
 
-  const reducer = require('./reducer');
-  const store = finalCreateStore(reducer, data);
+  const finalCreateStore = compose(...enhancers)(_createStore);
+  const missingReducers = getMissingReducers(createReducers(), data);
+  const store = finalCreateStore(combineReducers(createReducers(missingReducers)), data);
+
+  store.asyncReducers = {};
+  store.inject = inject.bind(null, store);
+
+  if (persistConfig) {
+    createPersistor(store, persistConfig);
+    store.dispatch({ type: 'PERSIST' });
+  }
 
   if (__DEVELOPMENT__ && module.hot) {
     module.hot.accept('./reducer', () => {
-      store.replaceReducer(require('./reducer'));
+      store.replaceReducer(combineReducers(require('./reducer').default(store.asyncReducers)));
     });
   }
 
--- src/redux/middleware/clientMiddleware.js
+++ src/redux/middleware/clientMiddleware.js
@@ -1,55 +1,26 @@
-/**
- * Converts ApiClient into a middleware
- * Expected action object:
-  {
-    types: [REQUEST_ACTION, SUCCESS_ACTION, FAILURE_ACTION],
-    onSuccess: SUCCESS_CALLBACK,
-    onError: FAILURE_CALLBACK,
-    promise: (client) => client[METHOD]
-  }
- *
- * @param {ApiClient}
- * @return {func} - returns redux middleware
- */
+export default function clientMiddleware({ client, app, restApp }) {
+  return ({ dispatch, getState }) => next => action => {
+    if (typeof action === 'function') {
+      return action(dispatch, getState);
+    }
 
-export default function clientMiddleware(client) {
-  return ({ dispatch, getState }) => {
-    return next => action => {
-      if (typeof action === 'function') {
-        return action(dispatch, getState);
-      }
+    const { promise, types, ...rest } = action; // eslint-disable-line no-redeclare
+    if (!promise) {
+      return next(action);
+    }
 
-      const {
-        promise,
-        types,
-        onSuccess = () => {},
-        onError = () => {},
-        ...rest
-      } = action;
+    const [REQUEST, SUCCESS, FAILURE] = types;
+    next({ ...rest, type: REQUEST });
 
-      if (!promise) {
-        return next(action);
-      }
+    const actionPromise = promise({ client, app, restApp }, dispatch);
+    actionPromise.then(
+      result => next({ ...rest, result, type: SUCCESS }),
+      error => next({ ...rest, error, type: FAILURE })
+    ).catch(error => {
+      console.error('MIDDLEWARE ERROR:', error);
+      next({ ...rest, error, type: FAILURE });
+    });
 
-      const [REQUEST, SUCCESS, FAILURE] = types;
-      next({ ...rest, type: REQUEST });
-
-      const actionPromise = promise(client);
-      actionPromise.then(
-        (result) => {
-          onSuccess(dispatch, result);
-          next({ ...rest, result, type: SUCCESS });
-        },
-        (error) => {
-          onError(dispatch, error);
-          next({ ...rest, error, type: FAILURE });
-        }
-      ).catch((error) => {
-        console.error('MIDDLEWARE ERROR:', error);
-        next({ ...rest, error, type: FAILURE });
-      });
-
-      return actionPromise;
-    };
+    return actionPromise;
   };
 }
