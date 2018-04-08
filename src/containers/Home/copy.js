e98d6ac1eb751ce748bda58d3fc06d559c8231a1
Kaiyu Hsu <im.kaiyu@gmail.com>
2018-04-07 23:06:41 -0700

    Add new app directories

--- src/containers/Home/copy.js
+++
@@ -1,4 +0,0 @@
-export default {
-  title: 'Learning is much better with friends',
-  details: 'Create private groups and broadcast your learnings. A place to debate, share knowledge, and collaborate because everything is better with community.'
-};
