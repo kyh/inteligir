ed84b6a85d44ec978a8be830022a08e58e0e8dd5
Kaiyu Hsu <im.kaiyu@gmail.com>
2018-07-16 23:34:07 -0700

    Add precommit

--- tools/heroku_postbuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Heroku postbuild script to manage compilation of our JavaScript app and build all necessary
-# assets.
-#
-# Note:
-# You can add any additional postbuild methods to this file.
-
-npm run build
