ee55e6e5a45506520b73f5b790ce1d2e81de3397
Kaiyu Hsu <im.kaiyu@gmail.com>
2018-08-03 23:22:55 -0700

    Update config

--- server/config/app-keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGODB_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  redisUrl: process.env.REDIS_URL,
-  awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-};
