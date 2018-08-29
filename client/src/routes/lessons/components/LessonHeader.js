24ea54fc39fac8ef6a05e796e09ddfafdd18788f
Kaiyu Hsu <im.kaiyu@gmail.com>
2018-08-28 22:47:55 -0700

    Add lesson header component

---
+++ client/src/routes/lessons/components/LessonHeader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default ({ lesson }) => {
+  if (!lesson) return null;
+
+  return (
+    <header className="intro">
+      <span className="intro-author">
+        By:{' '}
+        {lesson.authors.map((a) => (
+          <Link to={`/profile/${a._id}`}>{a.displayName}</Link>
+        ))}
+      </span>
+      <h3 className="intro-title">{lesson.title}</h3>
+    </header>
+  );
+};
