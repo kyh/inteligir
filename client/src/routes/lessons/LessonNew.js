20a873fa24e7876d45e9df137bd6ebae521aefa7
Kaiyu Hsu <im.kaiyu@gmail.com>
2018-09-02 22:44:26 -0700

    Add draftjs packages

--- client/src/routes/lessons/LessonNew.js
+++ client/src/routes/lessons/LessonNew.js
@@ -1,32 +1,14 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
-import LessonForm from './components/LessonForm';
-import LessonFormReview from './components/LessonFormReview';
+import LessonEditor from './components/editor/LessonEditor';
 
 class LessonNew extends Component {
-  state = { showFormReview: false };
-
-  renderContent() {
-    if (this.state.showFormReview) {
-      return (
-        <LessonFormReview
-          onCancel={() => this.setState({ showFormReview: false })}
-        />
-      );
-    }
-
-    return (
-      <LessonForm
-        onLessonSubmit={() => this.setState({ showFormReview: true })}
-      />
-    );
-  }
-
   render() {
-    return <section className="padded-page">{this.renderContent()}</section>;
+    return (
+      <section className="padded-page">
+        <LessonEditor />
+      </section>
+    );
   }
 }
 
-export default reduxForm({
-  form: 'lessonForm',
-})(LessonNew);
+export default LessonNew;
