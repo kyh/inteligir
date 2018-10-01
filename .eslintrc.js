01e5369f99520b944e740c41fedfa52c7f0f7597
Kaiyu Hsu <im.kaiyu@gmail.com>
2018-10-01 14:47:36 -0700

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 const path = require('path');
+const npmPackage = require('./package.json');
+
+const MODULE_ALIAS = [
+  ...Object.keys(npmPackage._moduleAliases).map((key) => {
+    return [key, npmPackage._moduleAliases[key]];
+  }),
+];
 
 module.exports = {
   parser: 'babel-eslint',
@@ -19,12 +26,12 @@ module.exports = {
     'react/jsx-one-expression-per-line': 'off',
     // Allow console logs in our codebase.
     'no-console': 'off',
-    // AirBnB opted to use operators at the beginning of line breaks.
-    'operator-linebreak': 'off',
+    // This is to stop eslint from throwing errors when using module_alias (@server).
+    'import/no-extraneous-dependencies': 'off',
   },
   settings: {
     'import/resolver': {
-      alias: true,
+      alias: { map: MODULE_ALIAS },
     },
   },
 };
