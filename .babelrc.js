55d9b85bde53b544ddeedcbbaba6e32e1f40e279
Kaiyu Hsu <im.kaiyu@gmail.com>
2018-10-05 20:24:40 -0700

    Fix babel imports

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,8 @@
+const npmPackage = require('./package.json');
+
 const presets = ['next/babel'];
 const plugins = [
+  ['module-resolver', { alias: npmPackage._moduleAliases }],
   [
     'styled-components',
     {
