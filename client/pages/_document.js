19beff3e601f431c0086236c1287956db865b037
Kaiyu Hsu <im.kaiyu@gmail.com>
2018-10-09 15:52:29 -0700

    Add server stylesheets

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />),
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <html lang="en">
@@ -99,6 +109,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Source+Serif+Pro:400,700"
             rel="stylesheet"
           />
+          {this.props.styleTags}
         </Head>
         <body>
           <Main />
