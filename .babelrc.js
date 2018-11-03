4878207cb26e35ffb39e6a4e3078ed3ee5fa4cd6
Kaiyu Hsu <im.kaiyu@gmail.com>
2018-11-03 15:19:02 -0700

    Add in flow

--- .babelrc.js
+++ .babelrc.js
@@ -3,6 +3,7 @@ const npmPackage = require('./package.json');
 const presets = ['next/babel'];
 const plugins = [
   ['module-resolver', { alias: npmPackage._moduleAliases }],
+  'transform-flow-strip-types',
   [
     'styled-components',
     {
