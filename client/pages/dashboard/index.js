aa88ec291348657844cc17122c26e4291f7d7080
Kaiyu Hsu <im.kaiyu@gmail.com>
2018-11-05 18:04:48 -0800

    Update dependencies

---
+++ client/pages/dashboard/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+import Logo from '@client/components/Logo';
+
+const Dashboard = () => {
+  return (
+    <section>
+      <Head>
+        <title>Inteligir | Dashboard</title>
+      </Head>
+      <Logo />
+    </section>
+  );
+};
+
+export default Dashboard;
