194cf2f11be783e44380faea0dea05300da18ceb
Kaiyu Hsu <im.kaiyu@gmail.com>
2018-12-19 19:19:21 -0800

    Update dev environment

---
+++ tools/heroku_mappings.js
@@ -0,0 +1,7 @@
+const DEPLOY_LOCATIONS = {
+  dev: { herokuName: 'inteligir-dev', value: 'dev' },
+  staging: { herokuName: 'inteligir-staging', value: 'staging' },
+  prod: { herokuName: 'inteligir-prod', value: 'prod' },
+};
+
+module.exports = DEPLOY_LOCATIONS;
