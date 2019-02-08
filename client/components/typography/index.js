96a51f39b0d2518fa89640ae381a06802a4557c1
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-02-07 19:28:41 -0800

    Add blockLink component

--- client/components/typography/index.js
+++ client/components/typography/index.js
@@ -3,3 +3,4 @@ export { default as Link } from './src/Link';
 export { default as ActiveLink } from './src/ActiveLink';
 export { default as Truncate } from './src/Truncate';
 export { default as Heading } from './src/Heading';
+export { default as BlockLink } from './src/BlockLink';
