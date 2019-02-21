3b0033484f69e4798d9bc3c89492a02a2687b550
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-02-21 00:14:53 -0800

    Update how to create packages

--- client/components/index.js
+++ client/components/index.js
@@ -1,36 +1,9 @@
-export {
-  Box,
-  Container,
-  Loading,
-  Logo,
-  Meta,
-  Navigation,
-  Page,
-  Image,
-  BackgroundImage,
-  GlobalStyles,
-  Footer,
-} from './base';
-
-export { Absolute, Relative, Flex, Hide } from './layout';
-export {
-  Text,
-  Link,
-  ActiveLink,
-  BlockLink,
-  Truncate,
-  Heading,
-} from './typography';
-export { Card, ListCard, PromotedCard } from './card';
-export { Badge } from './badge';
-export { Tooltip } from './tooltip';
-export { List } from './list';
-export { HeroBackground, FooterBackground } from './images';
-export {
-  ConnectIcon,
-  EarnIcon,
-  SquaresIcon,
-  FacebookIcon,
-  TwitterIcon,
-  GoogleIcon,
-} from './icons';
+export * from './base';
+export * from './layout';
+export * from './typography';
+export * from './card';
+export * from './badge';
+export * from './tooltip';
+export * from './list';
+export * from './images';
+export * from './icons';
