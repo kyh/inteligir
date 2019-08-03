f7e1d4c47195e68f965ed4eafb208675e09de41a
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-08-02 18:01:16 -0700

    update navigation

--- client/containers/auth/Navigation.js
+++ client/containers/auth/Navigation.js
@@ -2,7 +2,8 @@ import React from 'react';
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import { withStyles } from '@material-ui/core/styles';
-import { Logo, Link } from '@components';
+import { Search } from '@material-ui/icons';
+import { Logo, Link, InputBase, InputAdornment } from '@components';
 
 import CurrentUser from './CurrentUser';
 import Logout from './Logout';
@@ -20,18 +21,33 @@ Router.onRouteChangeError = () => {
 
 const styles = (theme) => ({
   container: {
-    maxWidth: theme.brand.maxWidth,
-    margin: '0 auto',
     padding: `0 ${theme.spacing(3)}px`,
+    borderBottom: `1px solid ${theme.brand.borderColor}`,
   },
   nav: {
     display: 'flex',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'space-between',
-    padding: `${theme.spacing(3)}px 0`,
+    height: theme.spacing(8),
+    maxWidth: theme.brand.maxWidth,
+    margin: '0 auto',
+    '& > .nav-section': {
+      display: 'flex',
+    },
+    '& > .left': {
+      width: '50%',
+      justifyContent: 'flex-start',
+    },
+    '& > .right': {
+      width: '50%',
+      justifyContent: 'flex-end',
+    },
     '& a, & button': {
+      fontWeight: 700,
       marginRight: theme.spacing(3),
       border: 'none',
+      display: 'flex',
+      alignItems: 'center',
       '&:hover': {
         color: theme.palette.primary.dark,
       },
@@ -39,33 +55,58 @@ const styles = (theme) => ({
         marginRight: 0,
       },
     },
-    '& .get-started': {
-      padding: theme.spacing(1),
-      border: `2px solid ${theme.palette.primary.main}`,
-      borderRadius: 4,
-      '&:hover': {
-        borderColor: theme.palette.primary.dark,
-      },
-    },
+  },
+  searchInput: {
+    fontSize: '16px',
+    background: '#f0f1f2',
+    height: '45px',
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+    borderRadius: '4px',
+    alignSelf: 'center',
   },
 });
 
 function Navigation({ classes }) {
+  const [values, setValues] = React.useState({
+    search: '',
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
   return (
     <CurrentUser>
       {({ data: { me } }) => (
         <section className={classes.container}>
           <nav className={classes.nav}>
-            <Link href="/">
-              <Logo />
-            </Link>
-            <div>
+            <div className="nav-section left">
+              <Link href="/courses">Courses</Link>
+              <InputBase
+                className={classes.searchInput}
+                id="search"
+                type="text"
+                placeholder="Search..."
+                value={values.search}
+                onChange={handleChange('search')}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <Search color="action" />
+                  </InputAdornment>
+                }
+              />
+            </div>
+            <div className="nav-section center">
+              <Link href="/">
+                <Logo />
+              </Link>
+            </div>
+            <div className="nav-section right">
               {!me && (
                 <>
                   <Link href="/login">Log in</Link>
-                  <Link className="get-started" href="/signup">
-                    Get Started
-                  </Link>
+                  <Link href="/request_invite">Request an invite</Link>
                 </>
               )}
               {me && (
