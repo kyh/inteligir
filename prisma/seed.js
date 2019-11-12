85fbd345b022f45e4d477a0d73a25f56bfc26d04
Kaiyu Hsu <im.kaiyu@gmail.com>
2019-11-11 22:30:14 -0800

    update packages

--- prisma/seed.js
+++ prisma/seed.js
@@ -10,11 +10,13 @@ async function main() {
       displayName: 'Kai',
       password: '$2b$10$dqyYw5XovLjpmkYNiRDEWuwKaRAvLaG45fnXE5b3KTccKZcRPka2m', // "secret42"
       playlists: {
-        create: {
-          title: 'How to contribute to Inteligir',
-          description: 'Step by step guide on how Inteligir was built',
-          published: true,
-        },
+        create: [
+          {
+            title: 'How to contribute to Inteligir',
+            description: 'Step by step guide on how Inteligir was built',
+            published: true,
+          },
+        ],
       },
     },
   });
