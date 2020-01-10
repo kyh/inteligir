2884f99c3d45410601aea4025335ce13d77ae4d3
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-01-09 23:34:46 -0800

    Update packages

--- server/schema/index.js
+++ server/schema/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable global-require */
 const { makeSchema } = require('nexus');
 const { nexusPrismaPlugin } = require('nexus-prisma');
+const path = require('path');
+const keys = require('@server/config/keys');
 
 const types = {
   ...require('./Query'),
@@ -13,4 +15,9 @@ const types = {
 module.exports = makeSchema({
   types,
   plugins: [nexusPrismaPlugin()],
+  shouldGenerateArtifacts: keys.nodeEnv === 'development',
+  outputs: {
+    schema: path.join(__dirname, 'generated/schema.graphql'),
+    typegen: path.join(__dirname, 'generated/nexus.ts'),
+  },
 });
