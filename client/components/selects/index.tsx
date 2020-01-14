0c199ff084e2e90cc2ae222ef05d76c117534bf3
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-01-13 23:35:21 -0800

    Update more ts files

---
+++ client/components/selects/index.tsx
@@ -0,0 +1,6 @@
+export { default as Checkbox } from './Checkbox';
+export { default as CheckboxGroup } from './CheckboxGroup';
+export { default as Radio } from './Radio';
+export { default as RadioGroup } from './RadioGroup';
+export { default as Select } from './Select';
+export { default as Switch } from './Switch';
