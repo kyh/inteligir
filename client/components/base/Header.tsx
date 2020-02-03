3fc2ea7875b004d59a8802ee93234f329331dc67
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-02-02 21:38:29 -0800

    Add components

--- client/components/base/Header.tsx
+++ client/components/base/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 
-const Header: React.FC = ({ children, ...props }) => {
+function Header({ children, ...props }) {
   return (
     <Typography gutterBottom component="h1" variant="h4" {...props}>
       {children}
--- client/components/base/Text.tsx
+++ client/components/base/Text.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 
-const Text: React.FC = ({ children, ...props }) => {
+function Text({ children, ...props }) {
   return (
     <Typography component="p" variant="body1" {...props}>
       {children}
