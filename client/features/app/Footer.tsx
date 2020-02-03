507bd2689c3c4d25fc7483055ef4a2328f3481a5
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-02-02 21:45:25 -0800

    Add in features

---
+++ client/features/app/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+
+import { Grid, Card, CardContent, Header } from '@components'
+
+const useStyles = makeStyles(() => ({}))
+
+function Footer() {
+  const classes = useStyles()
+
+  return <footer></footer>
+}
+
+export default Footer
