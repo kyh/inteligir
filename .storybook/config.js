c7b97e56eff133c7ac8edc0ec6c2c965841cf956
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-07-31 02:12:23 -0700

    reset project

--- .storybook/config.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { StylesProvider } from '@material-ui/styles'
-import { addDecorator } from '@storybook/react'
-
-const StylesDecorator = (storyFn) => <StylesProvider injectFirst>{storyFn()}</StylesProvider>
-
-addDecorator(StylesDecorator)
