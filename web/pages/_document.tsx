1060bcce6ccb3a6a20b6533df83ae2ce8008612d
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-08-01 22:29:10 -0700

    update seo metadata

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -1,42 +1,47 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
 
-const NAME = "Inteligir";
-const TITLE = "Inteligir";
-const DESCRIPTION = "Your bite-sized micro lesson portal";
-const URL = "https://inteligir.com";
-
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
         <Head>
           <meta name="referrer" content="origin" />
-          <meta name="application-name" content={NAME} />
-          <meta name="theme-color" content="#1F2937" />
-          <meta name="title" content={TITLE} />
+          <meta name="application-name" content="Inteligir" />
+          <meta name="theme-color" content="#111827" />
           <meta name="robots" content="index, follow" />
-          <meta name="description" content={DESCRIPTION} />
-
           <meta property="fb:app_id" content="{FB_ID}" />
-          <meta property="og:url" content={URL} />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content={TITLE} />
-          <meta property="og:image" content="/featured.png" />
-          <meta property="og:description" content={DESCRIPTION} />
-          <meta property="og:site_name" content={NAME} />
-          <meta property="og:locale" content="en_US" />
 
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@kaiyuhsu" />
-          <meta name="twitter:creator" content="@kaiyuhsu" />
-          <meta name="twitter:url" content="" />
-          <meta name="twitter:title" content={TITLE} />
-          <meta name="twitter:description" content={DESCRIPTION} />
-          <meta name="twitter:image" content="/featured.png" />
-
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="icon" href="/logo192.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#111827"
+          />
+          <link rel="shortcut icon" href="/favicon/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#00aba9" />
+          <meta
+            name="msapplication-config"
+            content="/favicon/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <ColorModeScript />
