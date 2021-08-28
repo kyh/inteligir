95342e5686cc0850c4b1aeb6deaac382ae499d8c
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-08-28 16:09:54 +0800

    style comments

--- web/data/comments.ts
+++ web/data/comments.ts
@@ -9,5 +9,84 @@ export const comments = [
     body: "What books do you have on your bookshelf just to look smarter than you actually are?",
     comments: 291,
   },
-  // More posts...
+  {
+    id: 2,
+    user: {
+      name: "Floyd Miles",
+      imageUrl:
+        "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    body: "What books do you have on your bookshelf just to look smarter than you actually are?",
+    comments: 291,
+  },
+  {
+    id: 3,
+    user: {
+      name: "Floyd Miles",
+      imageUrl:
+        "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    body: "What books do you have on your bookshelf just to look smarter than you actually are?",
+    comments: 291,
+  },
+  {
+    id: 4,
+    user: {
+      name: "Floyd Miles",
+      imageUrl:
+        "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    body: "What books do you have on your bookshelf just to look smarter than you actually are?",
+    comments: 291,
+  },
+  {
+    id: 5,
+    user: {
+      name: "Floyd Miles",
+      imageUrl:
+        "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    body: "What books do you have on your bookshelf just to look smarter than you actually are?",
+    comments: 291,
+  },
+  {
+    id: 6,
+    user: {
+      name: "Floyd Miles",
+      imageUrl:
+        "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    body: "What books do you have on your bookshelf just to look smarter than you actually are?",
+    comments: 291,
+  },
+  {
+    id: 7,
+    user: {
+      name: "Floyd Miles",
+      imageUrl:
+        "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    body: "What books do you have on your bookshelf just to look smarter than you actually are?",
+    comments: 291,
+  },
+  {
+    id: 8,
+    user: {
+      name: "Floyd Miles",
+      imageUrl:
+        "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    body: "What books do you have on your bookshelf just to look smarter than you actually are?",
+    comments: 291,
+  },
+  {
+    id: 9,
+    user: {
+      name: "Floyd Miles",
+      imageUrl:
+        "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    body: "What books do you have on your bookshelf just to look smarter than you actually are?",
+    comments: 291,
+  },
 ];
--- web/pages/home.tsx
+++ web/pages/home.tsx
@@ -21,61 +21,59 @@ export const HomePage = () => {
       <SEO />
       <Container>
         <Sidebar />
-        <main className="md:mx-20">
+        <main className="md:mx-16">
           <TopbarHeader className="sticky top-0">
-            <Heading className="mb-0">Home</Heading>
+            <Heading>Home</Heading>
           </TopbarHeader>
-          <ul role="list" className="my-5">
+          <ul role="list">
             {lessons.map((lesson) => (
               <Card as="li" key={lesson.id}>
                 <article aria-labelledby={"question-title-" + lesson.id}>
-                  <div>
-                    <div className="flex space-x-3">
-                      <div className="flex-shrink-0">
-                        <img
-                          className="w-10 h-10 rounded-full"
-                          src={lesson.author.imageUrl}
-                          alt=""
-                        />
-                      </div>
-                      <div className="flex-1 min-w-0">
-                        <p className="text-sm font-medium text-gray-900">
-                          <a
-                            href={lesson.author.href}
-                            className="hover:underline"
-                          >
-                            {lesson.author.name}
-                          </a>
-                        </p>
-                        <p className="text-sm text-gray-500">
-                          {lesson.hashtags.map((hashtag) => (
-                            <a
-                              key={hashtag}
-                              href={lesson.href}
-                              className="mr-1 hover:underline"
-                            >
-                              #{hashtag}
-                            </a>
-                          ))}
-                        </p>
-                      </div>
-                      <div className="flex self-center flex-shrink-0">
-                        <Button
-                          variant="outline"
-                          size="xs"
-                          className={cx(lesson.following && "opacity-50")}
-                        >
-                          {lesson.following ? "Following" : "Follow"}
-                        </Button>
-                      </div>
+                  <div className="flex space-x-3">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="w-10 h-10 rounded-full"
+                        src={lesson.author.imageUrl}
+                        alt=""
+                      />
                     </div>
-                    <h2
-                      id={"question-title-" + lesson.id}
-                      className="mt-4 text-base font-medium text-gray-900"
-                    >
-                      {lesson.title}
-                    </h2>
+                    <div className="flex-1 min-w-0">
+                      <p className="text-sm font-medium text-gray-900">
+                        <a
+                          href={lesson.author.href}
+                          className="hover:underline"
+                        >
+                          {lesson.author.name}
+                        </a>
+                      </p>
+                      <p className="text-sm text-gray-500">
+                        {lesson.hashtags.map((hashtag) => (
+                          <a
+                            key={hashtag}
+                            href={lesson.href}
+                            className="mr-1 hover:underline"
+                          >
+                            #{hashtag}
+                          </a>
+                        ))}
+                      </p>
+                    </div>
+                    <div className="flex self-center flex-shrink-0">
+                      <Button
+                        variant="outline"
+                        size="xs"
+                        className={cx(lesson.following && "opacity-50")}
+                      >
+                        {lesson.following ? "Following" : "Follow"}
+                      </Button>
+                    </div>
                   </div>
+                  <h2
+                    id={"question-title-" + lesson.id}
+                    className="mt-4 text-base font-medium text-gray-900"
+                  >
+                    {lesson.title}
+                  </h2>
                   <div
                     className="mt-2 space-y-4 text-sm text-gray-700"
                     dangerouslySetInnerHTML={{ __html: lesson.body }}
@@ -146,44 +144,49 @@ export const HomePage = () => {
             <TopbarHeader className="flex justify-end">
               <Topbar />
             </TopbarHeader>
-            <section aria-labelledby="comments-section" className="my-5">
-              <Card>
-                <Heading id="comments-section" as="h2" className="text-lg">
-                  Comments
-                </Heading>
-                <List role="list">
-                  {comments.map((comment) => (
-                    <li key={comment.id} className="flex py-4 space-x-3">
-                      <div className="flex-shrink-0">
-                        <img
-                          className="w-8 h-8 rounded-full"
-                          src={comment.user.imageUrl}
-                          alt={comment.user.name}
-                        />
+            <section
+              aria-labelledby="comments-section"
+              className="my-6 overflow-auto"
+            >
+              <Heading
+                id="comments-section"
+                as="h2"
+                className="sticky top-0 pb-2 mb-0 text-lg bg-gradient-to-b from-white"
+              >
+                Comments
+              </Heading>
+              <List role="list">
+                {comments.map((comment) => (
+                  <li key={comment.id} className="flex py-4 space-x-3">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="w-8 h-8 rounded-full"
+                        src={comment.user.imageUrl}
+                        alt={comment.user.name}
+                      />
+                    </div>
+                    <div className="flex-1 min-w-0">
+                      <p className="text-sm text-gray-800">{comment.body}</p>
+                      <div className="flex mt-2">
+                        <span className="inline-flex items-center text-sm">
+                          <button
+                            type="button"
+                            className="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
+                          >
+                            <FiMessageCircle
+                              className="w-5 h-5"
+                              aria-hidden="true"
+                            />
+                            <span className="font-medium text-gray-900">
+                              Reply
+                            </span>
+                          </button>
+                        </span>
                       </div>
-                      <div className="flex-1 min-w-0">
-                        <p className="text-sm text-gray-800">{comment.body}</p>
-                        <div className="flex mt-2">
-                          <span className="inline-flex items-center text-sm">
-                            <button
-                              type="button"
-                              className="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
-                            >
-                              <FiMessageCircle
-                                className="w-5 h-5"
-                                aria-hidden="true"
-                              />
-                              <span className="font-medium text-gray-900">
-                                Reply
-                              </span>
-                            </button>
-                          </span>
-                        </div>
-                      </div>
-                    </li>
-                  ))}
-                </List>
-              </Card>
+                    </div>
+                  </li>
+                ))}
+              </List>
             </section>
           </SidebarFixed>
         </aside>
