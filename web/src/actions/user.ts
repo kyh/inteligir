a73098dca88922b778bcea79d98c65c7ca288855
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-09-02 17:17:08 +0800

    migrate firebase to 9

--- web/src/actions/user.ts
+++ web/src/actions/user.ts
@@ -1,8 +1,17 @@
 import { firestore, useQuery, prepareDocForCreate } from "util/db";
+import {
+  collection,
+  query,
+  where,
+  doc,
+  addDoc,
+  deleteDoc,
+} from "firebase/firestore";
 
 export const useUserBlocks = (uid = "") => {
   const blockedUsersQuery = useQuery(
-    uid && firestore.collection("userBlocks").where("createdBy", "==", uid)
+    uid &&
+      query(collection(firestore, "userBlocks"), where("createdBy", "==", uid))
   );
 
   const dataMap = blockedUsersQuery.data
@@ -21,11 +30,13 @@ export const useUserBlocks = (uid = "") => {
   return { status: blockedUsersQuery.status, dataMap };
 };
 
-export const blockUser = (userId: string) => {
-  const block = prepareDocForCreate({ blockedUser: userId });
-  return firestore.collection("userBlocks").add(block);
+export const blockUser = (userId = "") => {
+  return addDoc(
+    collection(firestore, "userBlocks"),
+    prepareDocForCreate({ blockedUser: userId })
+  );
 };
 
-export const unblockUser = (blockId: string) => {
-  return firestore.collection("userBlocks").doc(blockId).delete();
+export const unblockUser = (blockId = "") => {
+  return deleteDoc(doc(collection(firestore, "userBlocks"), blockId));
 };
