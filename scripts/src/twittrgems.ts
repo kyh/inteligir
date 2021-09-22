cbf02c3dbe1e09f9764b4327f2e14407e150493e
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-09-23 00:00:20 +0800

    finish saving gems

--- scripts/src/twittrgems.ts
+++ scripts/src/twittrgems.ts
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 import axios, { Method } from "axios";
 import URL from "url-parse";
-import { getTweet } from "./twitter";
+// import { getTweet } from "./twitter";
 
 const GEM_TOKEN = process.env.TWITTRGEM_TOKEN;
 
@@ -20,7 +20,7 @@ type Gem = {
   __v: number;
 };
 
-const getGems = async (page = 1) => {
+const getRawGems = async (page = 1) => {
   const config = {
     method: "GET" as Method,
     url: "https://upksilll-web.herokuapp.com/posts",
@@ -43,14 +43,70 @@ const extractTweetId = (gem: Gem) => {
   return paths[paths.length - 1];
 };
 
+type SavedGem = {
+  gem_id: string;
+  tweet_id: string;
+  link: string;
+  tags: string;
+};
+
+const getSavedGems = async () => {
+  const config = {
+    method: "GET" as Method,
+    url: "https://v1.nocodeapi.com/kaiyu/google_sheets/pCKJEbKOzytqoCjN?tabId=Sheet1",
+  };
+
+  const response = await axios(config);
+
+  return response.data.data.reduce(
+    (map: Record<string, SavedGem>, savedGem: SavedGem) => {
+      map[savedGem.gem_id] = savedGem;
+      return map;
+    },
+    {}
+  );
+};
+
+const insertGems = async (gems: Gem[]) => {
+  const config = {
+    method: "POST" as Method,
+    url: "https://v1.nocodeapi.com/kaiyu/google_sheets/pCKJEbKOzytqoCjN?tabId=Sheet1",
+    data: gems.map((g) => [
+      g._id,
+      extractTweetId(g),
+      g.link,
+      JSON.stringify(g.category),
+    ]),
+  };
+
+  const response = await axios(config);
+  return response.data;
+};
+
 const main = async () => {
-  const { data } = await getGems();
-  const gem = data[0];
-  console.log("gem:", gem);
-  const tweetId = extractTweetId(gem);
-  console.log("tweetId:", tweetId);
-  const { data: tweet } = await getTweet(tweetId);
-  console.log("tweet:", tweet);
+  const savedMap = await getSavedGems();
+  console.log("saved gems:", Object.keys(savedMap));
+  let pageNo = 1;
+  let totalPages = 100;
+
+  while (pageNo <= totalPages) {
+    console.log(`getting raw gems for page ${pageNo}...`);
+    const response = await getRawGems(pageNo);
+    console.log(`got ${response.data.length} gems`);
+
+    for (const gem of response.data) {
+      if (savedMap[gem._id]) {
+        console.log("already saved:", gem._id);
+      } else {
+        console.log("saving gem:", gem);
+        const response = await insertGems([gem]);
+        console.log("response:", response);
+      }
+    }
+
+    pageNo = response.pageNo + 1;
+    totalPages = response.totalPages;
+  }
 };
 
 if (require.main === module) {
