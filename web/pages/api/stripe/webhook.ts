e82ebd1e6f8f28903bc0e4adbee3650ae2fe339f
Kai Yu Hsu <kaihsu@amazon.com>
2021-10-14 16:35:16 +0800

    rename files

--- web/pages/api/stripe/webhook.ts
+++ web/pages/api/stripe/webhook.ts
@@ -1,9 +1,9 @@
 import { PriceType, SubscriptionStatus } from "@prisma/client";
 import Stripe from "stripe";
-import { timestampToDate } from "@util/common";
-import { createApiHandler } from "@server/createApiHandler";
+import { createApiHandler } from "@server/handler";
 import { stripe } from "@server/stripe";
 import { prisma } from "@server/prisma";
+import { timestampToDate } from "@util/common";
 
 // Stripe requires the raw body to construct the event.
 export const config = {
