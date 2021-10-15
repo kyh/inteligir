81ba12543212078213edd4a31218323079d78768
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-10-15 20:39:58 +0800

    update dcokerfiles

--- web/pages/api/auth/[...nextauth].ts
+++ web/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,6 @@ import { NextApiHandler } from "next";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "@server/prisma";
 
 const options: NextAuthOptions = {
@@ -15,27 +14,6 @@ const options: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    }),
-    CredentialsProvider({
-      credentials: {
-        // username: { label: "Username", type: "text" },
-        // password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: 1, name: "J Smith", email: "jsmith@example.com" };
-
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null or false then the credentials will be rejected
-          return null;
-          // You can also Reject this callback with an Error or with a URL:
-          // throw new Error('error message') // Redirect to error page
-          // throw '/path/to/redirect'        // Redirect to a URL
-        }
-      },
     }),
   ],
   adapter: PrismaAdapter(prisma),
