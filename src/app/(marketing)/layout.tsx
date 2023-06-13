0317251aa88fc4d29609647ae164c03f1cbeb8b5
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-06-13 09:39:36 -0700

    fix build errors

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -3,6 +3,8 @@ import loadUserData from "~/lib/server/loaders/load-user-data";
 import { FooterNavigation } from "./components/FooterNavigation";
 import { TopNavigation } from "./components/TopNavigation";
 
+export const dynamic = "force-dynamic";
+
 function SiteLayout({ children }: React.PropsWithChildren) {
   const data = use(loadUserData());
 
