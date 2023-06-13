7a0f9565dae597ace8674dbc9d76d09ee724de11
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-06-13 01:06:03 -0700

    fix blurring effect

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -13,8 +13,8 @@ export const metadata = {
 const ErrorPage = () => {
   return (
     <main>
-      <TopNavigation userSession={undefined}/>
-      <div className="m-auto flex min-h-[50vh] w-full items-center justify-center">
+      <TopNavigation userSession={undefined} />
+      <div className="m-auto flex min-h-screen w-full items-center justify-center">
         <div className="flex flex-col space-y-8">
           <div className="flex space-x-8 divide-x divide-zinc-100">
             <div>
@@ -24,7 +24,9 @@ const ErrorPage = () => {
             </div>
             <div className="flex flex-col space-y-4 pl-8">
               <div className="flex flex-col space-y-2">
-                <Text as="h2" variant="heading2">Oooops. An error occurred</Text>
+                <Text as="h2" variant="heading2">
+                  Oooops. An error occurred
+                </Text>
                 <p className="text-zinc-500 dark:text-zinc-300">
                   Apologies, an error occurred while processing your request.
                   Please contact us if the issue persists.
