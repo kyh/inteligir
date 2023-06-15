7050e68d7864819b7b772f291a6bd7c5f97808d2
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-06-15 07:53:43 -0700

    update typewriter effect

--- src/app/(marketing)/components/HeroExample.tsx
+++ src/app/(marketing)/components/HeroExample.tsx
@@ -9,7 +9,6 @@ import {
   useScroll,
   useTransform,
 } from "framer-motion";
-import Typewriter, { TypewriterClass } from "typewriter-effect";
 import { Badge } from "~/components/Badge";
 import styles from "./HeroExample.module.css";
 
@@ -68,24 +67,21 @@ export const HeroExample = () => {
 
 const samples = [
   {
-    input:
-      "I'm writing a story about the gaming industry, who should I talk to?",
+    input: "Can you provide a changelog of everything shipped in TC this week?",
     output: {
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nisi harum unde tenetur obcaecati quaerat laboriosam reprehenderit totam sequi! Corrupti, commodi odit officia praesentium repellat harum quaerat quia explicabo amet.",
       references: ["Reference 1", "Reference 2"],
     },
   },
   {
-    input:
-      "I'm writing a story about the gaming industry, who should I talk to?",
+    input: "Who is responsible for the design of the landing experience?",
     output: {
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nisi harum unde tenetur obcaecati quaerat laboriosam reprehenderit totam sequi! Corrupti, commodi odit officia praesentium repellat harum quaerat quia explicabo amet.",
       references: ["Reference 1", "Reference 2"],
     },
   },
   {
-    input:
-      "I'm writing a story about the gaming industry, who should I talk to?",
+    input: "Whats our HR policy on working from home?",
     output: {
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nisi harum unde tenetur obcaecati quaerat laboriosam reprehenderit totam sequi! Corrupti, commodi odit officia praesentium repellat harum quaerat quia explicabo amet.",
       references: ["Reference 1", "Reference 2"],
@@ -94,54 +90,96 @@ const samples = [
 ];
 
 export const ExampleChat = ({ start }: { start: boolean }) => {
-  const [instance, setInstance] = useState<TypewriterClass>();
   const [currentSampleIndex, setCurrentSampleIndex] = useState(0);
-  const [currentOutputText, setCurrentOutputText] = useState("");
-  const [currentReferences, setCurrentReferences] = useState([]);
+  const [showAnswer, setShowAnswer] = useState(false);
 
-  useEffect(() => {
-    const currentSample = samples[currentSampleIndex];
-    if (instance && start) {
-      instance
-        .typeString(currentSample.input)
-        .callFunction(() => {
-          setCurrentOutputText(currentSample.output.text);
-        })
-        .start();
-    }
-    if (instance && !start) {
-      instance.deleteAll().callFunction(() => {
-        setCurrentOutputText("");
-        setCurrentReferences([]);
-      });
-    }
-  }, [instance, start, currentSampleIndex]);
+  const currentSample = samples[currentSampleIndex];
 
   return (
     <section className={styles.container}>
       <section className={styles.window}>
         <div className={styles.input}>
           <Typewriter
-            options={{
-              delay: 50,
-            }}
-            onInit={(typewriter) => {
-              setInstance(typewriter);
-            }}
+            start={start}
+            text={currentSample.input}
+            onTyped={() => setShowAnswer(true)}
+            onCleared={() => setShowAnswer(false)}
           />
         </div>
         <div className={styles.loading} />
         <div className={styles.output}>
-          <div className={styles.outputText}>{currentOutputText}</div>
+          <div className={styles.outputText}>
+            {showAnswer && currentSample.output.text}
+          </div>
           <footer className={styles.outputFooter}>
             <div className="flex gap-2">
-              {currentReferences.map((reference) => (
-                <Badge key={reference}>{reference}</Badge>
-              ))}
+              {showAnswer &&
+                currentSample.output.references.map((reference) => (
+                  <Badge key={reference}>{reference}</Badge>
+                ))}
             </div>
           </footer>
         </div>
       </section>
     </section>
+  );
+};
+
+const Typewriter = ({
+  start,
+  text,
+  onTyped,
+  onCleared,
+}: {
+  start: boolean;
+  text: string;
+  onTyped?: () => void;
+  onCleared?: () => void;
+}) => {
+  const [finished, setFinished] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let i = 0;
+
+    const write = () => {
+      if (!ref.current || finished) return;
+      if (i < text.length) {
+        ref.current.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(write, 50);
+      } else {
+        i = 0;
+        onTyped?.();
+        setFinished(true);
+      }
+    };
+
+    const clear = () => {
+      if (!ref.current || !finished) return;
+      if (i < text.length) {
+        ref.current.innerHTML = text.substring(0, text.length - i);
+        i++;
+        setTimeout(clear, 5);
+      } else {
+        i = 0;
+        ref.current.innerHTML = "";
+        onCleared?.();
+        setFinished(false);
+      }
+    };
+
+    if (start) {
+      write();
+    } else {
+      clear();
+    }
+  }, [ref, start, text, onTyped, onCleared, finished]);
+
+  return (
+    <div>
+      <span ref={ref} />
+      <span className={styles.cursor}>|</span>
+    </div>
   );
 };
