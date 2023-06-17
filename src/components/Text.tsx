0531a3db718bf82088df2ccbb4e09f7a2645d111
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-06-17 07:01:27 -0700

    only set leading none on larger screens

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -4,7 +4,7 @@ export const Text = classed("span", {
   base: "text-base",
   variants: {
     variant: {
-      heading1: "text-3xl font-semibold leading-none sm:text-4xl",
+      heading1: "text-3xl font-semibold sm:text-4xl md:leading-none",
       heading2: "text-xl font-semibold sm:text-2xl",
       label: "text-xs font-semibold uppercase tracking-wider",
     },
