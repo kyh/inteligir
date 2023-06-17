0825bb1ffb0cfc75c4796b802d52e65f5b733df2
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-06-17 07:02:22 -0700

    remove leading none

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -4,7 +4,7 @@ export const Text = classed("span", {
   base: "text-base",
   variants: {
     variant: {
-      heading1: "text-3xl font-semibold sm:text-4xl md:leading-none",
+      heading1: "text-3xl font-semibold sm:text-4xl",
       heading2: "text-xl font-semibold sm:text-2xl",
       label: "text-xs font-semibold uppercase tracking-wider",
     },
