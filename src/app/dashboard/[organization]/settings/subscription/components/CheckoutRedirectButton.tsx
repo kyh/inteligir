4109f7093bf73afd66f947d9aab8e90686ba9b8f
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-06-28 01:19:30 -0400

    0.2.4

--- src/app/dashboard/[organization]/settings/subscription/components/CheckoutRedirectButton.tsx
+++ src/app/dashboard/[organization]/settings/subscription/components/CheckoutRedirectButton.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import ArrowRightIcon from "@heroicons/react/24/outline/ArrowRightIcon";
 import isBrowser from "~/core/generic/is-browser";
 import useCsrfToken from "~/core/hooks/use-csrf-token";
+import { createCheckoutAction } from "~/lib/stripe/actions";
 import { cn } from "~/lib/utils/cn";
 import { Button } from "~/components/Button";
 
@@ -11,14 +12,14 @@ const CheckoutRedirectButton: React.FCC<{
   disabled?: boolean;
   stripePriceId?: string;
   recommended?: boolean;
-  organizationId: Maybe<number>;
+  organizationUid: string;
   customerId: Maybe<string>;
 }> = ({ children, ...props }) => {
   return (
-    <form data-cy="checkout-form" action="/api/stripe/checkout" method="POST">
+    <form data-cy="checkout-form" action={createCheckoutAction}>
       <CheckoutFormData
         customerId={props.customerId}
-        organizationId={props.organizationId}
+        organizationUid={props.organizationUid}
         priceId={props.stripePriceId}
       />
       <Button
@@ -39,7 +40,7 @@ export default CheckoutRedirectButton;
 
 function CheckoutFormData(
   props: React.PropsWithChildren<{
-    organizationId: Maybe<number>;
+    organizationUid: string;
     priceId: Maybe<string>;
     customerId: Maybe<string>;
   }>
@@ -50,14 +51,12 @@ function CheckoutFormData(
     <>
       <input
         type="hidden"
-        name="organizationId"
-        defaultValue={props.organizationId}
+        name="organizationUid"
+        defaultValue={props.organizationUid}
       />
-
       <input type="hidden" name="csrf_token" defaultValue={csrfToken} />
       <input type="hidden" name="returnUrl" defaultValue={getReturnUrl()} />
       <input type="hidden" name="priceId" defaultValue={props.priceId} />
-
       <input type="hidden" name="customerId" defaultValue={props.customerId} />
     </>
   );
