11b4c2fe1140f29b607ee7b214c80a79cef77d9b
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-07-01 01:51:27 -0400

    update to latest

--- src/lib/server/loaders/load-auth-page-data.ts
+++ src/lib/server/loaders/load-auth-page-data.ts
@@ -1,8 +1,4 @@
 import "server-only";
-import {
-  getURLFromRedirectError,
-  isRedirectError,
-} from "next/dist/client/components/redirect";
 import { redirect } from "next/navigation";
 import verifyRequiresMfa from "~/core/session/utils/check-requires-mfa";
 import getSupabaseServerClient from "~/core/supabase/server-client";
@@ -14,31 +10,21 @@ import getSupabaseServerClient from "~/core/supabase/server-client";
  * to the authentication pages.
  */
 const loadAuthPageData = async () => {
-  try {
-    const client = getSupabaseServerClient();
+  const client = getSupabaseServerClient();
 
-    const {
-      data: { session },
-    } = await client.auth.getSession();
+  const {
+    data: { session },
+  } = await client.auth.getSession();
 
-    const requiresMultiFactorAuthentication = await verifyRequiresMfa(client);
+  const requiresMultiFactorAuthentication = await verifyRequiresMfa(client);
 
-    // If the user is logged in and does not require multi-factor authentication,
-    // redirect them to the home page.
-    if (session && !requiresMultiFactorAuthentication) {
-      return redirect("/dashboard");
-    }
-
-    return {};
-  } catch (e) {
-    if (isRedirectError(e)) {
-      return redirect(getURLFromRedirectError(e));
-    }
-
-    console.error(e);
-
-    return {};
+  // If the user is logged in and does not require multi-factor authentication,
+  // redirect them to the home page.
+  if (session && !requiresMultiFactorAuthentication) {
+    redirect("/dashboard");
   }
+
+  return {};
 };
 
 export default loadAuthPageData;
