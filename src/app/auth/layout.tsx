7eaea3d1e97b62a228315750d595919dd8d701ea
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-07-01 01:51:27 -0400

    update to latest

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,11 +1,10 @@
-import { use } from "react";
 import loadAuthPageData from "~/lib/server/loaders/load-auth-page-data";
 import AuthPageShell from "~/app/auth/components/AuthPageShell";
 
 export const dynamic = "force-dynamic";
 
-function AuthLayout({ children }: React.PropsWithChildren) {
-  const data = use(loadAuthPageData());
+async function AuthLayout({ children }: React.PropsWithChildren) {
+  await loadAuthPageData();
 
   return <AuthPageShell>{children}</AuthPageShell>;
 }
