c88287a7f15077801577ed9bc89f02551c2c0603
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-09-29 16:47:16 -0700

    update packages

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -4,11 +4,8 @@ import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
-
-  // Supabase client to handle cookies
   const supabase = createMiddlewareClient({ req, res });
 
-  // Refresh expired sessions
   await supabase.auth.getSession();
 
   return res;
