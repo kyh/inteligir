49db29acfabde9c816eec51508899e352ca5a0f4
Kaiyu Hsu <im.kaiyu@gmail.com>
2026-05-15 22:27:20 -0700

    chore: remove unused sonner+vaul; wire up base-ui toast wrapper

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { GlobalAlertDialog } from "@repo/ui/components/alert-dialog";
-import { Toaster } from "@repo/ui/components/sonner";
+import { Toaster } from "@repo/ui/components/toast";
 import { TooltipProvider } from "@repo/ui/components/tooltip";
 
 import { siteConfig } from "@/lib/site-config";
